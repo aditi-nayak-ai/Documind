3fb3dde7e0660b824cd59a5602f31937235561b5
Aditi Nayak <153495639+aditi-nayak-ai@users.noreply.github.com>
2026-09-25 12:02:18 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+ 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+  },
   server: {
     proxy: {
       "/api": {
@@ -13,3 +16,4 @@ export default defineConfig({
     },
   },
 });
+ 
